0e9d5c31c2ffa70fce6a8c10153bae295a7a4ea6
Santiago Vizcaino <jorge_santiagovizcaino@hotmail.com>
2026-07-09 15:29:54 -0500

    feat: crear entidades BaseEntity y User en MsUsers.Domain

---
+++ Services/MsItem/MsItem.Domain/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿namespace MsItem.Domain.Entities;
+
+
+/// <summary>
+/// Entidad User - Representa un usuario del sistema
+/// </summary>
+public class User : BaseEntity
+{
+    public required string Username { get; set; }      
+    public string? Email { get; set; }
+    public required string FullName { get; set; }
+    public bool IsActive { get; set; }
+
+    public int TotalAssignedItems { get; set; } = 0;
+    public int CompletedItems { get; set; } = 0;
+    public int PendingItems { get; set; } = 0;
+    public int HighRelevanceItems { get; set; } = 0;
+
+    public bool IsSaturated() => HighRelevanceItems >= 3;
+}
